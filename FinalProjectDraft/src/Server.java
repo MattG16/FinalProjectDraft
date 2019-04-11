49d1629ffe0db0763b521e1f673d499eb37e7d2a
mateigolesteanu <mateigolesteanu@10.0.0.242>
2019-04-10 20:07:51 -0400

    3rd Commit
    Working command line echo server

--- FinalProjectDraft/src/Server.java
+++ FinalProjectDraft/src/Server.java
@@ -1,16 +1,44 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 	
 	public static void main(String[] args) throws IOException {
 		
 		ServerSocket ss = new ServerSocket(5112);
-		Socket connect = ss.accept();
 		
+		Socket connected = ss.accept();
 		
+		OutputStream out = connected.getOutputStream();
+		
+		InputStream in = connected.getInputStream();
+		
+		Scanner input = new Scanner(in, "UTF-8");
+		
+		PrintWriter output = new PrintWriter(new OutputStreamWriter(out));
+		
+	//	out.writeChars("Hello there client");
+		
+		boolean done = false;
+		
+		while(!done) {
+	
+				String line = input.nextLine();
+			//	System.out.println("Got here");
+				output.println(line);
+				output.flush();
+				if(line.trim().equals("BYE")) {
+					done = true;
+				}
+			
+			
+		}
 		
 	}
 	
