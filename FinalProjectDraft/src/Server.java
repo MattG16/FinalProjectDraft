9eca617e622dbd3e2e928587b924214b0eb3b6ce
mateigolesteanu <mateigolesteanu@10.0.0.242>
2019-04-27 10:47:43 -0400

    Reworked structure of the Server class

--- FinalProjectDraft/src/Server.java
+++ FinalProjectDraft/src/Server.java
@@ -1,5 +1,6 @@
 import java.io.BufferedInputStream;
 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
@@ -11,14 +12,19 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
 	
-	public static void main(String[] args) throws Exception {
+	public static Set<PrintWriter> w = new HashSet<>();
 	
+	public static void main(String[] args) throws Exception {
+		
 		try(ServerSocket ss = new ServerSocket(5112);) {
 			ExecutorService pool = Executors.newFixedThreadPool(20);
 			while(true) {
@@ -26,7 +32,94 @@ public class Server {
 			}
 		} 
 		
-		
 	}
 	
+private static class ServerThread implements Runnable {
+
+		private Socket connected;
+		
+		ServerThread(Socket socket) {
+			this.connected = socket;
+		}
+			
+		@Override
+		public void run() {
+			
+			
+			System.out.println("Client connected");
+			
+			InputStream in;
+			
+			try {
+				in = connected.getInputStream();
+				
+				InputStreamReader inReader = new InputStreamReader(in);
+				
+				Scanner reader = new Scanner(connected.getInputStream());
+				
+				OutputStream out = connected.getOutputStream();
+				
+				OutputStreamWriter outWriter = new OutputStreamWriter(out);
+				
+				PrintWriter writer = new PrintWriter(connected.getOutputStream(), true);
+				
+				boolean done = false;
+				
+				String line = "";
+			
+				w.add(writer);
+				
+				while(!done) {
+					
+				//	System.out.println("Executed");
+					
+				//	if(reader.ready()) {
+						
+					//	System.out.println("Execute");
+						
+						line = reader.nextLine();
+						
+						System.out.println("Input from client: " + line);
+						
+						for(PrintWriter wr : w) {
+							wr.println(line);
+						}
+						
+						writer.flush();
+						
+				//	}
+					
+					
+					
+				}
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				
+				try {
+					connected.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
+		
+			
+			
+
+			
+		}
+
+	}
+
+	
 }
+
+
+
+
